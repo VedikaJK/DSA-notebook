20955c9a71d1c6f63d255a069f0b9af744266fa6
Vedika Kulkarni <50884920+VedikaJK@users.noreply.github.com>
2023-09-03 08:26:51 +0530

    Update sp_floydwarshall.cpp

--- Graph/sp_floydwarshall.cpp
+++ Graph/sp_floydwarshall.cpp
@@ -1,1 +1,46 @@
 
+// Online IDE - Code Editor, Compiler, Interpreter
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int>> floydwarshall(vector<vector<int>> graph, int n){
+
+    vector<vector<int>> dist = graph;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            for(int k=0; k<n; k++){
+                if( dist[j][i]!=INT_MAX && dist[i][k]!=INT_MAX && dist[j][k]>dist[j][i]+dist[i][k] ){
+                    dist[j][k] = dist[j][i]+dist[i][k];
+                }
+            }
+        }
+    }
+
+    return dist;
+
+}
+
+int main()
+{
+    // cout<<"Welcome to Online IDE!! Happy Coding :)";
+
+    vector<vector<int>> graph = { { 0, 5, 2, INT_MAX },
+                        { 5, 0, 1, 1 },
+                        { 2, 1, 0, 4 },
+                        { INT_MAX, 1, 4, 0 } }; 
+    
+    vector<vector<int>> SP = floydwarshall(graph,4);
+
+    for(int i=0; i<4; i++){
+        for(int j=0; j<4; j++){
+            cout<<SP[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
+
